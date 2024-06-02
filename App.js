52d1c2bdcbb6fbe19aee533d15dbb3f22b1215b8
Shivam Kumar <shivamkumar0170951@gmail.com>
2024-06-02 10:40:16 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+function changeBg(color){
+    document.body.style.backgroundColor = color;
+    if(color == "black"){
+        document.body.style.color = "white"
+    }else {
+        document.body.style.color = "black" 
+    }
+}
